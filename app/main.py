e43aa943a762e6bd257650b266f87b630a18f6f6
Jagdeshkumar422 <jk9876211@gmail.com>
2025-02-08 17:31:03 +0500

    updated code

--- app/main.py
+++ app/main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
-from app.graphql_schema import routes, graphql_schema
+from app.graphql_schema import schema
+from app import routes
 from strawberry.fastapi import GraphQLRouter
 
 
 app = FastAPI()
 
-graphql_app = GraphQLRouter(graphql_schema)
+graphql_app = GraphQLRouter(schema)
 app.include_router(graphql_app, prefix="/graphql")
 app.include_router(routes.router)
 @app.get("/")
